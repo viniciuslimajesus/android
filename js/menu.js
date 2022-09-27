d7ee8d63683afb1ef118f8bcc34aabca8bc10e08
viniciuslimajesus <vinicius.jesus.vl157@gmail.com>
2022-09-27 00:02:33 -0300

    Adicionei um novo CSS

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,11 @@ function clickMenu() {
         menu.style.display = 'block'
     }
 }
+
+function mudouTamanho() {
+    if(window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
